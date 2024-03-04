8382cfab4953b034755df76643dc7831194f0f63
Braden Durian <braden.durian@csuglobal.edu>
2024-03-03 20:58:54 -0700

    second commit, add methods for menu items

--- application/Main.java
+++ application/Main.java
@@ -1,30 +1,46 @@
 package application;
 	
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
+	
+	//Declare field for output text
+	private TextField dateTimeField;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		try {
-			GridPane gridPane = new GridPane();				//Positions components within scene
-			Scene scene = new Scene(gridPane);				//Scene contains all content
+			GridPane root = new GridPane();				//Positions components within scene
+			Scene scene = new Scene(root);				//Scene contains all content
 			Label dateLabel = new Label("Date & Time:");	//Label for Date and Time field
-			TextField dateTimeField = new TextField();		//Field for Date and Time
+			dateLabel.setLabelFor(dateTimeField);
+			dateTimeField = new TextField();		//Field for Date and Time
 			
 			//Set title of stage
 			primaryStage.setTitle("Module 3");
@@ -32,35 +48,89 @@ public class Main extends Application {
 			//Create MenuBar
 			MenuBar menuBar = new MenuBar();
 			
-			/*final VBox vbox = new VBox();
-			*vbox.setAlignment(Pos.CENTER);
-			*vbox.setSpacing(10);
-			*vbox.setPadding(new Insets(0, 10, 0, 10));
-			*vbox.getChildren().addAll(null
-			*/
 			//Create menu and menu items
 			Menu menu = new Menu("Menu");
 			MenuItem dateTime = new MenuItem("Date & Time");
-			dateTime.setOnAction((ActionEvent t) -> {
-				
-			});
 			MenuItem saveText = new MenuItem("Save Text to Log");
-			MenuItem randomGreen = new MenuItem("Change Hue of Green");
+			MenuItem randomGreen = new MenuItem("Change Background Color");
 			MenuItem exit = new MenuItem("Exit");
 			
+			//Add menu items to menu
 			menu.getItems().addAll(dateTime, saveText, randomGreen, exit);
 			
+			//Add menu to menubar
+			menuBar.getMenus().addAll(menu);
+			
+			//Add ActionEvent handling to menu items FIXME create methods for actions
+			dateTime.setOnAction(e -> printDateTime());
+			saveText.setOnAction(e -> writeToFile());
+			randomGreen.setOnAction(e -> changeBackgroundColor(root));
+			exit.setOnAction(e -> primaryStage.close());
+			
+			//Configure text area
+			dateTimeField.setEditable(false);
+			dateTimeField.setPrefHeight(100);
+			
+			//Add MenuBar and dateTimeField to root layout
+			root.add(menuBar, 0, 0);
+			root.add(dateTimeField, 1, 1);
+			root.add(dateLabel, 0, 1);
+			root.setPadding(new Insets(10));
 			
 			
-			
-			
+			//Set scene and show
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	//Method for printing local date and time
+	private void printDateTime() {
+		//Save local date and time
+		LocalDateTime now = LocalDateTime.now();
+		
+		//Create formatter with desired style
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+		
+		//Save formatted dateTime string 
+		String formattedDateTime = now.format(formatter);
+		
+		//Output formatted date time in dateTimeField
+		dateTimeField.appendText(formattedDateTime + "\n");
+	}
+	
+	//Method for writing text area to file
+	private void writeToFile() {
+		try {
+			//Write contents of dateTimeField to log.txt
+			FileWriter writer = new FileWriter("log.txt", true);
+			writer.write(dateTimeField.getText());
+			writer.close();
+			Alert alert = new Alert(AlertType.INFORMATION, "Contents written to log.txt");
+			alert.showAndWait();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Method for changing background color
+	private void changeBackgroundColor(GridPane root) {
+		//Create random
+		Random random = new Random();
+		
+		//Generate random number for hue. Green is between 81 and 140. 140 - 81 = 59 so that is the range for our hue value
+		int hue = random.nextInt(59);
+		
+		//Create color object with hue + 81.0 to encompass all hue degrees for green
+		Color color = Color.hsb(hue + 81.0, 1.0, 0.50);
+		
+		root.setStyle("-fx-background-color: " + color.toString().replace("0x", "#") + ";");
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
